6f193558c3ac2b7e862fdd65473d4f7ba1b98e55
Efendi-Sugiantoro-16 <efendisugiantoro16@gmail.com>
2026-01-31 20:46:47 +0700

    fixing endpoint

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -2,6 +2,8 @@ class AppConstants {
   // Use 10.0.2.2 for Android Emulator to access localhost
   // Use localhost or your IP for iOS/Web
   // static const String baseUrl = 'https://php-api-production-f25.up.railway.app/api';
+  // static const String baseUrl =
+  //     'http://10.0.2.2:8000/api'; // Use 10.0.2.2 for Android Emulator
   static const String baseUrl =
-      'http://10.0.2.2:8000/api'; // Use 10.0.2.2 for Android Emulator
+      'https://php-api-production-f25.up.railway.app/api';
 }
